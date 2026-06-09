7e61d628ad368f87185179e858422efe49547651
Alaa-fawaz-Manea-AF <alaafawaz010011@gmail.com>
2026-06-10 02:10:02 +0300

    finish Full_Stack and deploy net flix colne

--- back_end/src/main.ts
+++ back_end/src/main.ts
@@ -12,9 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonConfig,
   });
-
   app.enableCors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   });
